35f462231be9de9e443f70e186262aaca2b8e2bd
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-04-13 23:42:46 -0400

    index.html modified for contact form, script.js modified for navigation menu features, style.css modified for navigation and contact form

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 // Display mobile menu
 const mobileMenu = () => {
@@ -8,3 +9,46 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu)
+
+// show active menu when scrolling
+const highlightMenu = () => {
+    const element = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const projectsMenu = document.querySelector('#projects-page')
+    let scrollPos = window.scrollY
+    
+
+    // adds 'highlight' class to menu items
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }   else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        projectsMenu.classList.remove('highlight')
+        return
+    }   else if (window.innerWidth > 960 && scrollPos < 2345) {
+        projectsMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+}
+if((element && window.innerWidth < 960 && scrollPos < 600) || element) {
+    element.classList.remove('highlight')
+}
+}
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+// close mobile menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+}
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
