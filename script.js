2b2310f95e96cfd58fe3366591fbac379be8f402
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-04-22 23:57:50 -0400

    fixed contact section html, styled contact section css, added effect for input and button javascript

--- script.js
+++ script.js
@@ -52,3 +52,22 @@ const hideMobileMenu = () => {
 }
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
+
+// contact form section; adding 'focus' function so that when input 
+// is selected it gets focused, and removes it when input is lost.
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add('focus');
+}
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+    parent.classList.remove('focus');
+    }
+}
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+})
